4e8aa96fdca40a4689a021f5f708060edb607b91
Sean Jaculina <sjaculina@horizon.csueastbay.edu>
2021-08-11 17:07:50 -0700

    implemented iframe tag

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,7 +5,12 @@ const VideoDetail = ({video}) =>{
         return <div>Loading...</div>
     }
 
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
     return (
+        <div>
+            <div className="ui embed">
+            <iframe src={videoSrc}/>
+            </div>
         <div className="ui segment">
             <h4 className="ui header">
                 {video.snippet.title}
@@ -13,7 +18,7 @@ const VideoDetail = ({video}) =>{
             <p>
                 {video.snippet.description}
             </p>
-
+        </div>
         </div>
     )
 }
